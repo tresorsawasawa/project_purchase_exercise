31f5a361388673e1c6de34160db6575334019710
tresorsawasawa <tresorsawasawa@gmail.com>
2025-01-21 09:56:28 +0200

    Setup the project

---
+++ av_project_purchase/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Av Operations",
+    "version": "17.0.0.0",
+    "author": "Tresor Sawasawa",
+    "category": "Operations",
+    "description": "This module is designed to improve Project and Purchase modules with new functionalities.",
+    "depends": ["base", "project", "purchase"],
+    "data": [
+        # Security
+
+        # Data
+
+        # views
+    ],
+    "installable": True,
+    "application": True,
+    "license": "LGPL-3"
+}
