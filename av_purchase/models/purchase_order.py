33eb45fb9e4a77270a86145b6004a187c00fb2ef
tresorsawasawa <tresorsawasawa@gmail.com>
2025-02-11 10:51:15 +0200

    [IMP] av_purchase: improve the code functionalities by following the changes required from the review

--- av_purchase/models/purchase_order.py
+++ av_purchase/models/purchase_order.py
@@ -25,26 +25,24 @@ class PurchaseOrder(models.Model):
             # Check if the department has a manager
             department_manager = order.create_uid.employee_id.department_id.manager_id
             if not department_manager:
-                raise UserError("The RFQ creator's department must have a manager assigned to it in order to submit an RFQ. Please reach out to the system administrator.")
+                raise UserError("The RFQ creator's department must have a manager assigned in order to submit an RFQ. Please reach out to the system administrator.")
 
-            
             # Ensure the department manager has an associated user
             if not department_manager.user_id:
-                raise UserError("The RFQ creator's department must have a manager assigned to it in order to submit an RFQ. Please reach out to the system administrator.")
-
-            # Create a To-Do activity for the HOD when a CP submits an RFQ
-            if order.user_id.has_group('av_purchase.group_purchase_cp'):
-                if department_manager.user_id:
-                    self.env['mail.activity'].create({
-                        'activity_type_id': self.env.ref('mail.mail_activity_data_todo').id,
-                        'res_id': order.id,
-                        'res_model_id': self.env['ir.model']._get_id('purchase.order'),
-                        'user_id': department_manager.user_id.id,
-                        'summary': "RFQ Submitted - Approval Needed",
-                        'note': "A new RFQ has been submitted and needs a review or an approval.",
-                    })
+                raise UserError(f"The department manager '{department_manager.name}' does not have an Odoo user account. Please contact the administrator to assign a user account before submitting an RFQ.")
 
             # If all validations pass, change the state to 'sent'
             order.write({'state': 'sent'})
 
-        return True
+            # Create a clickable link to the RFQ
+            rfq_link = f"<a href='#id={order.id}&model=purchase.order'>{order.name}</a>"
+
+            # Create a To-Do activity for the HOD when a CP submits an RFQ
+            self.env['mail.activity'].create({
+                'activity_type_id': self.env.ref('mail.mail_activity_data_todo').id,
+                'res_id': order.id,
+                'res_model_id': self.env.ref('purchase.model_purchase_order').id,
+                'user_id': department_manager.user_id.id,
+                'summary': f"RFQ To Review",
+                'note': f"A new RFQ {rfq_link} has been submitted and needs a review or an approval.",
+            })
